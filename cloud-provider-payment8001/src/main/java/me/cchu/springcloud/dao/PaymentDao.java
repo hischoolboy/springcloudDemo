be6d9c7044682b7dc0c880407f6584a118d3adec
cchu <hischoolboy@163.com>
2021-03-03 19:24:24 +0800

    update

---
+++ cloud-provider-payment8001/src/main/java/me/cchu/springcloud/dao/PaymentDao.java
@@ -0,0 +1,11 @@
+package me.cchu.springcloud.dao;
+import me.cchu.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PaymentDao {
+    int create(Payment payment);
+
+    Payment getPaymentById(@Param("id") Long id);
+}
