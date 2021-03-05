2fb64b4fff705852d9e1642f8a980de18f4d400b
cchu <hischoolboy@163.com>
2021-03-05 21:11:02 +0800

    spring cloud alibaba nacos 集成

---
+++ cloud-alibaba-config-nacos-client-3377/src/main/java/client/controller/ConfigClientController.java
@@ -0,0 +1,19 @@
+package client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope // 支持 nacos 的动态刷新
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
