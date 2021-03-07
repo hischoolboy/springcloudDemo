e813ab13dc38a42edb17f93f155b81317b5e09b5
hischoolboy <hischoolboy@gmail.com>
2021-03-07 17:45:15 +0800

    seata集成

--- cloud-alibaba-consumer-nacos-order-84/src/main/java/springcloud/OrderNacosMain84.java
+++ cloud-alibaba-consumer-nacos-order-84/src/main/java/springcloud/OrderNacosMain84.java
@@ -1,15 +1,16 @@
-package me.cchu.springcloud;
-
+package springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableDiscoveryClient
-public class PaymentMain9004 {
+@SpringBootApplication
+@EnableFeignClients
+public class OrderNacosMain84 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain9004.class, args);
+        SpringApplication.run(OrderNacosMain84.class, args);
     }
 }
