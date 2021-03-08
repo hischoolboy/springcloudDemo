01b6140495e24faf4aa0981dd682918f46a82c91
cchu <hischoolboy@163.com>
2021-03-08 22:18:56 +0800

    更新配置

--- cloud-alibaba-config-nacos-client-3377/src/main/java/client/controller/ConfigClientController.java
+++ cloud-alibaba-config-nacos-client-3377/src/main/java/client/controller/ConfigClientController.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope // 支持 nacos 的动态刷新
+//@RefreshScope // 支持 nacos 的动态刷新
 @RestController
 public class ConfigClientController {
 
